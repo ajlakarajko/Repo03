12f2132e27910bc4b0444970867778549c12ae17
Ajla Fijuljanin <ajlafi@maestralsolutions.com>
2022-07-07 09:32:32 +0200

    Day03

---
+++ ex02/src/index2.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+    // Amra je carica!
+    /* document.write("Amra je kraljica!");
+    document.write("Amra je faraonka!");
+    */
+    // Only change code above this line
+    }
+    console.log(main());
+    module.exports = main;

44db26ddd2074a543e6e2e7a01c5bed56e376144
Ajla Fijuljanin <ajlafi@maestralsolutions.com>
2022-07-12 12:21:15 +0200

    Day03

---
+++ ex08/src/index09.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    var sum;
+    var numOne;
+    var numTwo;
+    sum = 23;
+    numOne = 15;
+    numTwo =8;
+    // Only change code above this line
+    return sum;
+    }
+    console.log(main(15, 8)); // change this line
+    module.exports = main;

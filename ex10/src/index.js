676c9eedee13d7bf1a7cc213b18c268ef282aae4
Ajla Fijuljanin <ajlafi@maestralsolutions.com>
2022-07-12 12:32:14 +0200

    Day03

---
+++ ex10/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    var product;
+    var numOne;
+    var numTwo;
+    product=45;
+    numOne=15;
+    numTwo=3;
+    // Only change code above this line
+    return product;
+    }
+    console.log(main(15, 3)); // change this line
+    module.exports = main;
